1a2b825d265e267684fd4f8cd2a139c755642161
Ricimello Lucien <lucien.ricimello@gmail.com>
2017-09-25 15:28:06 +0000

    correct issue when init pin mode

--- motor/apimotor/apimotor.py
+++ motor/apimotor/apimotor.py
@@ -5,12 +5,17 @@ import sys
 
 class Motor:
     def __init__(self, pin_speed, pin_pos_mot, pin_neg_mot,
-                 polarity=False, pullpos=0, pullneg=0, pullspeed=0, coeff=1)
+                 polarity=False, pullpos=0, pullneg=0, pullspeed=0, coeff=1):
         #pins
+        if GPIO.getmode() == None:
+            GPIO.setmode(GPIO.BCM)
         self.pinspeed = pin_speed
-        self.pwmspeed = GPIO.PWM(pin_speed, 1)
         self.posmot = pin_pos_mot
         self.negmot = pin_neg_mot
+        GPIO.setup(self.posmot, GPIO.OUT)
+        GPIO.setup(self.negmot, GPIO.OUT)
+        GPIO.setup(self.pinspeed, GPIO.OUT)
+        self.pwmspeed = GPIO.PWM(pin_speed, 1)
         # default rotation direction
         self.polarity = polarity
         # if a motor is slower or faster than other,
@@ -20,9 +25,6 @@ class Motor:
         self.pullp = pullpos
         self.pulln = pullneg
         self.pulls = pullspeed
-        GPIO.setup(self.pinspeed, GPIO.OUT)
-        GPIO.setup(self.posmot, GPIO.OUT)
-        GPIO.setup(self.negmot, GPIO.OUT)
         return
 
     def stop():
@@ -42,8 +44,6 @@ class Motor:
             GPIO.output(self.posmot, GPIO.HIGH)
 
         return
-
-    def direction
 
     def reset():
         self.stop()
