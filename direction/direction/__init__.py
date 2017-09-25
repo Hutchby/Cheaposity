9a1c9b464bce1e44044fd262220a8a34361b01b1
Ricimello Lucien <lucien.ricimello@gmail.com>
2017-09-25 14:07:11 +0000

    staged

---
+++ direction/direction/__init__.py
@@ -0,0 +1,22 @@
+from apimotor import *
+
+class direction:
+
+    def __init__(self):
+        self.dir
+        self.speed
+        return
+
+    def start():
+        return
+
+    # stop the rover, reduce speed to 0
+    def stop():
+        return
+
+    def rotate():
+        return
+
+
+
+if __name__ == '__main__':
