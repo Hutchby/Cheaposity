09f9a5c68ce5d2aa048d0101af01ec105df3db78
lucien.ricimello <lucien.ricimello@epita.fr>
2017-10-23 10:00:25 +0000

    add some teste

--- direction/direction/__init__.py
+++ direction/direction/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from apimotor import *
 from evdev import InputDevice, categorize, ecodes
 
@@ -20,9 +21,10 @@ mconf is the number of motor used
 
 sign = lambda x: math.copysign(1, x)
 
-class direction:
+class Direction:
 
-    def __init__(self, M1, M4, M2=None, M3=None, config=2, speed=0, direction=0):
+    def __init__(self, M1=None, M4=None, M2=None, M3=None,
+                 config=2, speed=0, direction=0):
         self.direction = direction
         self.speed = speed
         self.mconf = config
@@ -36,10 +38,10 @@ class direction:
 
 
     def apply(self, s1, s2, s3, s4):
-        if direction == 0 || direction == 2:
+        if mconf == 0 or mconf == 2:
             self.m1.run(s1)
             self.m4.run(s4)
-        if direction == 1 || direction == 2:
+        if mconf == 1 or mconf == 2:
             self.m2.run(s2)
             self.m3.run(s3)
         return
@@ -85,6 +87,9 @@ class direction:
 
 if __name__ == '__main__':
     print("test")
+    rover=Direction(M1=Motor(21, 20, 16), M4=Motor(12, 24, 25), config=0)
+
+    '''
     gamepad = InputDevice('/dev/input/event0')
     aBtn = 315
     bBtn = 311
@@ -122,3 +127,4 @@ if __name__ == '__main__':
                 elif event.code == Trig:
                     print("Bumper")
                     self.clean()
+'''
