f89a23e8eeb71c3d0b7b21a6e3e83832011e9474
Guillaume Doré <dore_g@epita.fr>
2017-11-06 18:05:24 +0100

    httpserver: pfe_server.py: add a do_GET_status method to return server status

--- src/httpserver/pfe_server.py
+++ src/httpserver/pfe_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from server_base import RequestHandler
+from http.server import HTTPServer
 
 class PFEServer(HTTPServer):
 
@@ -14,12 +15,29 @@ class PFEServer(HTTPServer):
         pass
 
     def do_POST_gps(self, *args, **kwargs):
-        self.drone_gps = kwargs['data'][0]
+        self.drone_gps = kwargs['data']
 
     def do_POST_mpu(self, *args, **kwargs):
-        self.drone_accel = kwargs['data'][0]['accel']
-        self.drone_gyro = kwargs['data'][0]['gyro']
-        self.drone_magneto = kwargs['data'][0]['magneto']
+        self.drone_accel = kwargs['data']['accel']
+        self.drone_gyro = kwargs['data']['gyro']
+        self.drone_magneto = kwargs['data']['magneto']
 
     def do_POST_bat(self, *args, **kwargs):
-        self.drone_battery = kwargs['data'][0]
+        self.drone_battery = kwargs['data']
+
+    def do_GET_status(self, *args, **kwargs):
+        return {
+                    'drone_state':
+                    {
+                        'battery': self.drone_battery,
+                        'accel': self.drone_accel,
+                        'gyro': self.drone_gyro,
+                        'magneto': self.drone_magneto,
+                        'gps': self.drone_gps,
+                    },
+               }
+
+
+if __name__ == "__main__":
+    server = PFEServer(("0.0.0.0", 8888), RequestHandler)
+    server.serve_forever()
