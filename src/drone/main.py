87cd47ef4f86cf44fc252ff0f4aed099fdb5024e
kylox <maxime.gaudron@epita.fr>
2017-11-06 14:05:21 +0000

    drone:http: add craft of json package

---
+++ src/drone/main.py
@@ -0,0 +1,6 @@
+from i2c import gyrodriver
+import package
+
+
+if __name__ == '__main__':
+    package.craft_package()
--- src/drone/package.py
+++ src/drone/package.py
@@ -1,14 +1,18 @@
-def get_MPU():
-    return 0
+from i2c import gyrodriver
+import json
+import time
+import http
+
+def get_MPU(mpu):
+    return {'accel': mpu.readAccel(), 'gyro': mpu.readGyro(), 'magneto': {"x": 0, "y":0, "z":0}}
 
 def get_GPS():
     return 0
-
 def get_MULT():
     return 0
 
+def craft_package():
+    mpu = gyrodriver.MPU9250()
+    package = {'mpu': get_MPU(mpu), 'gps': {'lat': 0, 'lon': 0}, 'bat': 0}
+    return package
 
-def craft_package:
-    mpu = get_MPU()
-    gps = get_GPS()
-    mult = get_MULT()
