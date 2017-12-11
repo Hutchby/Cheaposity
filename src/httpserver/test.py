1f52d4ef3f3bc0219a49074f373dc029ac74011c
Guillaume Doré <dore_g@epita.fr>
2017-12-11 20:21:18 +0100

    httpserver: test.py: add more tests

--- src/httpserver/test.py
+++ src/httpserver/test.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 
 import requests
+import json
+from pprint import pprint
+
+SERVER_ADDR = 'http://0.0.0.0:8888/'
+
+REQUESTS = [
+        (requests.post, '', {'bat': 100}),
+        (requests.post, '', {'gps': 'Test with string'}),
+        (requests.post, '', {'mpu': {'accel': 10, 'gyro': 15, 'magneto': 20},}),
+        (requests.get, 'status', {}),
+        ]
 
 if __name__ == "__main__":
     # This request is supposed to work well with the server
-    post_data = {'print': 'Test message'}
-    r = requests.post("http://0.0.0.0:8888", data=post_data)
-    print(r.text)
+    for rq in REQUESTS:
+        r = rq[0](SERVER_ADDR + rq[1], data=json.dumps(rq[2]))
+        pprint(json.loads(r.text))
