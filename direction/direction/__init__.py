a5c0bf5933b13fd3ba486ed5bac76e267091f8c9
lucien.ricimello <lucien.ricimello@epita.fr>
2017-12-12 19:53:14 +0000

    api:motor: rebase

---
+++ direction/direction/__init__.py
@@ -0,0 +1,23 @@
+from apimotor import *
+
+class direction:
+
+    def __init__(self):
+        self.dir
+        self.speed
+        return
+
+    def start():
+        return
+
+    # stop the rover, reduce speed to 0
+    def stop():
+        return
+
+    def rotate():
+        return
+
+
+
+if __name__ == '__main__':
+    print("test")
