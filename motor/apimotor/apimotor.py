2e2b0086e39f37f62860f0062ab823fa13cb8bcb
lucien.ricimello <lucien.ricimello@epita.fr>
2017-12-12 19:44:13 +0000

    api:motor: correct algo apply on acceleration

--- motor/apimotor/apimotor.py
+++ motor/apimotor/apimotor.py
@@ -16,32 +16,59 @@ class Motor:
         self.adapwm.set_pwm_freq(60)
         self.pwmmin = 250
         self.pwmmax = 500
-        self.pwmmean = int((self.pwmmax + self.pwmmin) / 2)
+        self.pwmmean = (self.pwmmax + self.pwmmin) / 2
+        self.pwmwide = self.pwmmax - self.pwmmin
         self.coeff = coeff
         return
 
-    
-    def convert(self, speed):
-        return self.pwmmean + int((self.pwmmax - self.pwmmin) * speed / 200)
+    def algo(self, speed):
+        if speed == 0:
+            return 0
+        sign = 1
+        if speed < 0:
+            sign = -1
+        return sign * 20 + speed * 4 / 5
 
-    
+
+    def topwm(self, speed):
+        return self.pwmmean + self.pwmwide * speed / 200
+
+    #var: speed = +-100%
     def pwm(self, speed):
-        self.speed = speed
-        self.adapwm.set_pwm(self.pin, 0, self.convert(speed)) # * self.coeff
+        #speed = self.algo(speed)
+        speed = self.topwm(speed)
+        self.speed = speed * self.coeff
+        self.adapwm.set_pwm(self.pin, 0, int(self.speed)) # * self.coeff
+        print("pin: ", self.pin, " pwm:", self.speed, "(", speed, ")")
         return
 
-    
+
     def start(self, speed):
         self.pwm(speed)
-        #print("pin: ", self.pin, " pwm:", self.convert(speed), "(", speed, ")")
         return
 
-    
+
     def stop(self):
         self.start(0)
         return
 
-    
+
     def run(self, speed):
         self.start(speed)
         return
+
+
+if __name__ == '__main__':
+    print("Demo: Motor test")
+
+    M1=Motor(pin=14, coeff=1)
+    M2=Motor(pin=15, coeff=1)
+
+    #'''
+    while True:
+        #rot = int(input("rotation : "))
+        speed1 = int(input("M1 : "))
+        speed2 = int(input("M2 : "))
+
+        M1.pwm(speed1)
+        M2.pwm(speed2)
