7130e4cfc3b809a327bb0be49db74d6e33959846
kylox <maxime.gaudron@epita.fr>
2017-12-12 19:27:57 +0000

    test: add sonar to servo test

--- src/examples/simpletest.py
+++ src/examples/simpletest.py
@@ -5,14 +5,30 @@
 
 # Import the PCA9685 module.
 from pwm import servo
+from gpio import sonar
+import time
 
 servo = servo.servoMG996R()
+sonar = sonar.Sonar()
 
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:
     # Move servo on channel O between extremes.
     servo.rotate(1)
+    time.sleep(0.5)
+    print(sonar.measure_average())
+
     servo.rotate(3/4)
+    time.sleep(0.5)
+    print(sonar.measure_average())
     servo.rotate(1/2)
+    time.sleep(0.5)
+    print(sonar.measure_average())
     servo.rotate(1/4)
+    time.sleep(0.5)
+    print(sonar.measure_average())
     servo.rotate(0)
+    time.sleep(0.5)
+    print(sonar.measure_average())
+
+
