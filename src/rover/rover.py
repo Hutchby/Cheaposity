e9eb86315354d6e64a3a7fb76947ae5aa96ea315
kylox <maxime.gaudron@epita.fr>
2017-12-13 15:47:21 +0000

    rover:run: add test to activate the wheel

--- src/rover/rover.py
+++ src/rover/rover.py
@@ -12,8 +12,8 @@ class Rover:
         self.lcd =  lcddriver.lcd()
         self.gyroaccel = gyrodriver.MPU9250()
         self.magneto = gyrodriver.AK8963()
-        self.left_motor = apimotor.Motor(channel=15, coeff=1)
-        self.right_motor = apimotor.Motor(channel=7, coeff=.88)
+        self.left_motor = apimotor.Motor(channel=15, coeff=0.5)
+        self.right_motor = apimotor.Motor(channel=7, coeff=0.8)
 
     def do_scan(self):
         print("do_scan()")
@@ -34,9 +34,9 @@ class Rover:
     def run(self):
         print("rover running...")
         while True:
-            #print(self.do_scan())
-            #print("tick")
+            print(self.do_scan())
+            print("tick")
             self.left_motor.pwm(100)
-            self.right_motor.run(100)
+            self.right_motor.pwm(100)
 
 
